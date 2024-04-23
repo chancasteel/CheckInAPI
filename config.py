efaab21c9e4972ed343d7565b394f2859bd69025
Chandler Casteel <71947560+chancasteel@users.noreply.github.com>
2024-04-23 10:29:26 -0500

    Cleared config.py
    Removed all private security information from config.py. Made it more open source

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
+#ENTER API CREDITIALS
 credentials = dict(
-    url = "https://duncanvilleisd.incidentiq.com",
-    authkey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJjNzdhNjg2My1jMTNkLTRmMzktYTk5NS01ZmUzN2FjM2Y5MTAiLCJzY29wZSI6Imh0dHBzOi8vZHVuY2FudmlsbGVpc2QuaW5jaWRlbnRpcS5jb20iLCJzdWIiOiJhYmUzMWQ3Ny1lMDVhLTQyYjctYmNlNC02NGUwY2M3ODAxOTMiLCJqdGkiOiIxNjQ3ZmE0NS1kZWQ2LWVjMTEtYjY1Ni0wMDE1NWQ5ODAzZjEiLCJpYXQiOjE2NTI5MDEwNzQuMjMzLCJleHAiOjE3NDc1OTU0NzQuMjR9.AUpuzI82trLWMDVUbg4PlGn3gyDwYNPevdzYSlh3iWE",
-    siteid = "c77a6863-c13d-4f39-a995-5fe37ac3f910"
+    url = "",
+    authkey = "",
+    siteid = ""
 )
 
 headers = dict(
@@ -18,13 +19,12 @@ ticket =  dict(
     fullname = '',
     userid = '',
     ticket_id_url = '',
-    pending_view = 'e47dbd75-49fb-ee11-aaf0-000d3a0e23bd'
+    pending_view = ''
 )
 
+#ADD IMAGE PATH
 path = dict(
-    image = 'DvilleLogo.png',
+    image = '',
     csv = '',
     pdf = '',
 )
-
-#Finish Config and Split Comment to seperate Python
